bdcd7a734c53d56a6807f4bfc6db4ff70d723a3b
THV <thomashvidal@gmail.com>
2020-12-13 16:58:09 -0800

    classactcal as of 2012-03-11

--- src/lexicalanalyzer.h
+++ src/lexicalanalyzer.h
@@ -3,76 +3,125 @@
  *
  *       Filename: lexicalanalyzer.h
  *
- *    Description: This module processes all the file data for the docketmaster
- *                 program.  It converts a stream of characters into the
- *                 various tokens.
- *        
- *        Version: 1.0.20
- *        Created: 08/18/2011 
- *  Last Modified: Sat Nov 28 22:43:37 2020
+ *    Description: The lexical analyzer module reads a data file and adds the
+ *                 tokens derived from the file into the rules, holidays, and
+ *                 events data structures.
+ *
+ *        Version: 1.0
+ *        Created: 09/29/2011
+ *  Last Modified: Sun 11 Mar 2012 01:33:27 PM PDT
  *       Compiler: gcc
  *
- *         Author: Thomas H. Vidal (THV), thomashvidal@gmail.com
- *   Organization: Dark Matter Computing
- *  
- *      Copyright: Copyright (c) 2011-2020, Thomas H. Vidal
- *        License: This file is part of DocketMaster.
+ *         Author: Thomas H. Vidal (THV), thomasvidal@hotmail.com
+ *   Organization: Dark Matter Software
  *
- *                 DocketMaster is free software: you can redistribute it
- *                 and/or modify it under the terms of the GNU General
- *                 Public License as published by the Free Software Foundation,
- *                 version 2 of the License.
+ *      Copyright: Copyright (c) 2011, Thomas H. Vidal
  *
- *                 DocketMaster is distributed in the hope that it will be
- *                 useful,but WITHOUT ANY WARRANTY; without even the implied
- *                 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- *                 PURPOSE.  See the GNU General Public License for
- *                 more details.
- *
- *                 You should have received a copy of the GNU General Public
- *                 License along with DocketMaster.  If not, see
- *                 <https://www.gnu.org/licenses/>.
- *	        Usage:  
- *    File Format: 
- *   Restrictions: 
+ *	        Usage: The calling module kicks off the routines in this module by
+ *	               sending the data file, file name, filetype, field names, and
+ *	               the appropriate data structure.
+ *    File Format: All the data files read in as of now are CSV files.
+ *   Restrictions: This module does not handle the opening, closing, or checking
+ *                 the data files.  That work is done in the builder module.
+ *                 This module expects to receive a proper file type, with
+ *                 proper fields, and the appropriate data structure to store
+ *                 the tokens in.
  * Error Handling: 
  *     References: 
- *          Notes: 
- * 
- * SPDX-License-Identifier: GPL-2.0-only
- ===============================================================================
+ *          Notes: TODO Instead of passing one data structure type, pass a
+ *                 a void pointer to the data type??? 
+ * =============================================================================
  */
 
 #ifndef _LEXICALANALYZER_H_INCLUDED_
 #define _LEXICALANALYZER_H_INCLUDED_
+
+
+/* #####   HEADER FILE INCLUDES   ########################################### */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "utilities.h"
+#include "errorhandler.h"
 
-/****************************************************************************
-* Paramaterized Macros                                                      *
-*                                                                           *
-****************************************************************************/
 
-/* ASCII2DECIMAL convert a ASCII character into a decimal. Macro does no error
+/* #####   EXPORTED MACROS   ################################################ */
+
+/* ASCII2DECIMAL convert an ASCII character into a decimal. Macro does no error
 checking (of course!), but anticipates an ascii character between '0'
 (ASCII 48) and 9 (ASCII 57). */
 
 #define ASCII2DECIMAL(x) ((x)-48)
 
-/****************************************************************************
-* Data Type Definitions & Declarations                                      *
-*                                                                           *
-****************************************************************************/
 
-FILE *HOLIDAY_FILE;
+/* #####   EXPORTED SYMBOLIC CONSTANTS   #################################### */
+
+
+/*------------------------------------------------------------------------------
+ *  CSV Field Codes
+ *----------------------------------------------------------------------------*/
+
+/*  Define field codes for the holiday CSV File */
+
+#define HF_MONTH "Month"
+#define HF_RTYPE "Rule Type"
+#define HF_RULE "Rule"
+#define HF_HOLIDAY "Holiday"
+#define HF_AUTHORITY "Authority"
+
+/* Define field coedes for the events CSV File */
+
+#define EF_EVENT "Event"
+#define EF_TRIGGER "Trigger"
+#define EF_COUNT "Count"
+#define EF_CT_PD "Ct Pd"
+#define EF_AUTHORITY "Authority"
+
+/*------------------------------------------------------------------------------
+ *  Sizes and numbers of records and fields 
+ *----------------------------------------------------------------------------*/
+
+#define MAXRECORDLENGTH = 500 /* Maximum Length (in characters of CSV File line,
+				 each line consists of one record. */
+#define MAXNUMFIELDS = 25 /* Maxinum number of fields in CSV File */
+#define MAXFIELDLEN = 25 /* Maximum length (in chars) of name of field */
+
+/*------------------------------------------------------------------------------
+ *  Field and Text delimiters
+ *----------------------------------------------------------------------------*/
+
+#define FDELIMITER ',' /* Field delimiter for the CSV files */
+#define TDELIMITER '\"' /* Text string delimiter for the CSV files */
+#define NEWLINE '\n' /*  Newline */
+#define NULCHAR '\0' /* nul string */
+#define EMPTYFIELD '\0'
+
+/*------------------------------------------------------------------------------
+ *  Token processing flags
+ *----------------------------------------------------------------------------*/
+
+/* True if the first field delimiter is reached. */
+const unsigned char BEGIN_FIELD = (1<<0);
+
+/* True if the first text delimiter is reached. */
+const unsigned char BEGIN_TSTRING = (1<<1);
+
+/* True if the end text delimter is reached. */
+const unsigned char END_TSTRING = (1<<2);
+
+/* True if the token was found */
+const unsigned char TOKEN_FOUND = (1<<7);
+
+
+/* #####   EXPORTED DATA TYPES   ############################################ */
+
+extern FILE *HOLIDAY_FILE;
 extern FILE *EVENT_FILE;
 extern FILE *EXTRAS_FILE;
+extern EventNode eventlist; 
 
-/****************************************************************************
-* Function prototypes                                                       *
-*                                                                           *
-*****************************************************************************
-****************************************************************************/
+
+/* #####   EXPORTED FUNCTION DECLARATIONS   ################################# */
 
 /****************************************************************************
 **************************   FUNCTION DECLARATION   *************************
@@ -126,50 +175,25 @@ extern FILE *EXTRAS_FILE;
 * identifies the statutory (or other) legal authority for the rule.         *
 ****************************************************************************/
 
-int parseholidays (FILE *holidays);
+int parsefile (FILE *infile, const char *filename, FILETYPE ftype, const
+               char *fields[], void *datastruct);
 
-/****************************************************************************
-**************************   FUNCTION DECLARATION   *************************
-* Name: getfile                                                             *
-*                                                                           *
-* Description: Opens a file for reading.                                    *
-*                                                                           *
-* Arguments: Character string representing the file name.                   *
-*                                                                           *
-* Returns: File handle (pointer to the file).                               *
-*                                                                           *
-****************************************************************************/
 
-FILE * getfile(char *file_name);
-
-/****************************************************************************
-**************************   FUNCTION DECLARATION   *************************
-* Name: checkfile                                                           *
-*                                                                           *
-* Description: Verifies the name and version of an opened file.             *
-*                                                                           *
-* Arguments: File handle.                                                   *
-*                                                                           *
-* Returns: Nothing right now, but may change to return an int or char       *
-*   reflecting the type of opened filed.                                    *
-*                                                                           *
-****************************************************************************/
-
-int checkfile (FILE *in_file);
-
-/****************************************************************************
-**************************   FUNCTION DECLARATION   *************************
-* Name: closefile                                                           *
-*                                                                           *
-* Description: Closes a file.                                               *
-*                                                                           *
-* Arguments: File handle (pointer to the file).                             *
-*                                                                           *
-* Returns: NULL pointer.                                                    *
-*                                                                           *
-****************************************************************************/
-
-FILE * closefile(FILE *close_file);
+/* 
+ * ===  FUNCTION  ==============================================================
+ *          Name:  parseevents
+ *   Description:  This function parses the holiday file and creates a linked
+ *   		   list, each node is a court event.
+ *     Arguments:  File handle to events file 
+ *       Returns:  Zero if successful.  Function will return a non-zero error
+ *       	   code on specified errors, but this functionality has not been
+ *       	   coded as yet. 
+ *     Algorithm:   
+ *    References:   
+ * 	   Notes:   
+ * =============================================================================
+ */
+int parseevents(FILE *events);
 
 /****************************************************************************
 *****************************************************************************
---
+++ src/utilities.h
@@ -0,0 +1,43 @@
+/*
+ * =============================================================================
+ *
+ *       Filename: utilities.h
+ *
+ *    Description: contains a variety of utility functions used by the docket
+ *
+ *        Version: 1.0
+ *        Created: 03/11/2012 11:42:50 AM
+ *  Last Modified: Sun 11 Mar 2012 11:44:29 AM PDT
+ *       Compiler: gcc
+ *
+ *         Author: Thomas H. Vidal (THV), thomasvidal@hotmail.com
+ *   Organization: Dark Matter Software
+ *      Copyright: Copyright (c) 2012, Thomas H. Vidal
+ *
+ *	        Usage: 
+ *    File Format: 
+ *   Restrictions: 
+ * Error Handling: 
+ *     References: 
+ *          Notes: 
+ * =============================================================================
+ */
+
+
+
+#include <stdlib.h>
+
+/* #####   HEADER FILE INCLUDES   ########################################### */
+
+/* #####   EXPORTED MACROS   ################################################ */
+
+/* #####   EXPORTED SYMBOLIC CONSTANTS   #################################### */
+
+/* #####   EXPORTED TYPE DEFINITIONS   ###################################### */
+
+/* #####   EXPORTED DATA TYPES   ############################################ */
+
+/* #####   EXPORTED VARIABLES   ############################################# */
+
+/* #####   EXPORTED FUNCTION DECLARATIONS   ################################# */
+
